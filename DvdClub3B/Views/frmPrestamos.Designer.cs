a0bb4fdc61702c166bacb00007d814dd58f11546
gilnaman <gilbal@hotmail.com>
2018-07-12 01:36:35 -0500

    Esta es una prueba

---
+++ DvdClub3B/Views/frmPrestamos.Designer.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Balam
+ * Date: 12/07/2018
+ * Time: 01:34 a.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace DvdClub3B.Views
+{
+	partial class frmPrestamos
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// frmPrestamos
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Name = "frmPrestamos";
+			this.Text = "Prestamos de DVD";
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ DvdClub3B/Views/frmPrestamos.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Balam
+ * Date: 12/07/2018
+ * Time: 01:34 a.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DvdClub3B.Views
+{
+	/// <summary>
+	/// Description of frmPrestamos.
+	/// </summary>
+	public partial class frmPrestamos : Form
+	{
+		public frmPrestamos()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
